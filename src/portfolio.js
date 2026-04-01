d644f2d408640e40b009e623a08d6c7db420349b
Yash Pawar <yash.pawar@theemcoe.org>
2026-04-01 14:08:13 +0530

    Update portfolio settings and repository link

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@
 
 // Website related settings
 const settings = {
-  isSplash: true, // Change this to false if you don't want Splash screen.
+  isSplash: false, // Change this to false if you don't want Splash screen.
 };
 
 //SEO Related settings
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
     "https://drive.google.com/file/d/1bXRknv_h-XI_3CQ3SGPteGODtvEb7YvI/view?usp=sharing",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+  portfolio_repository: "https://github.com/yashpawar9274/Portfolios",
   githubProfile: "https://github.com/yashpawar9274",
 };
 
